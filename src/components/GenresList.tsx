62d11c26853d25f0af639e698258af5522298e4a
vinub <vinubunny@gmail.com>
2024-04-18 22:56:24 -0700

    Cleanup the Genres

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -10,6 +10,7 @@ import {
   Text,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/getCroppedImageUrl";
 
@@ -24,28 +25,35 @@ const GenresList = ({ handleClick, selectedGenre }: Props) => {
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              src={getCroppedImageUrl(genre.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-              objectFit="cover"
-            />
-            <Button
-              fontSize="lg"
-              variant="link"
-              onClick={() => handleClick(genre)}
-              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign="left"
+                fontSize="lg"
+                variant="link"
+                onClick={() => handleClick(genre)}
+                fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
